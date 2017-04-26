25736ac500fe9018cf42efdeee0cdc78c540df37
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-25 21:32:01 -0700

    adding calculator and practice

---
+++ practice.py
@@ -0,0 +1,43 @@
+"""
+This practice script is Part 2 of the Hackbright Prep functions exercise.
+Please write your functions below.
+"""
+
+# Write your code for hello_print
+def hello_print():
+    print "Hello"
+
+hello_print()
+
+
+
+
+
+# Write your code for hello_return
+def hello_return():
+    return "hello"
+
+hello_return()
+
+greet = hello_return()
+
+print greet
+
+
+
+
+
+
+# Write your code for hello_name
+navigator_name = "Letania"
+
+def hello_name(name):
+    print "hello{}".format(name)
+
+    
+
+hello_name(navigator_name)
+
+greet_again = hello_name("Sang")
+print greet_again
+
